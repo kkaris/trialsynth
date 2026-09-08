be437eeba7f8e2916c10bdb5f4005d86438b5ebb
kkaris <karis.klas@gmail.com>
2026-09-08 12:36:38 -0700

    Clean up pyproject.toml

---
+++ src/trialsynth/base/extract/__init__.py
@@ -0,0 +1,10 @@
+"""TrialSynth base extract package
+
+This submodule requires the `boto3` package. Install it with:
+
+    pip install boto3
+
+or include it when installing the `trialsynth` package with:
+
+    pip install trialsynth[llm]
+"""
